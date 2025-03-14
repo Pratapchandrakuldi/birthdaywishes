9904b3d18f86208f86d57e3b4d2577044b7ef695
Pratap Chandra kuldi <147026821+Pratapchandrakuldi@users.noreply.github.com>
2025-03-15 03:11:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,11 @@
-// import React from "react";
-// import "./App.css";
-// import { BdayCard, CakeCard, RituCard } from "../components/card";
-
-// const imagesPath = "/images/";
-
-// const images = [
-//   `${imagesPath}card1.jpg`, `${imagesPath}card4.jpg`, `${imagesPath}card2.jpg`,
-//   `${imagesPath}card3.jpg`, `${imagesPath}card5.jpg`, `${imagesPath}card6.jpg`,
-//   `${imagesPath}card9.jpg`, `${imagesPath}card14.jpg`, `${imagesPath}card15.jpg`,
-//   `${imagesPath}card10.jpg`, `${imagesPath}card11.jpg`
-// ];
-
-// const App = () => {
-//   return (
-//     <div className="app-container">
-//       <h1 className="title">🎉 Happy Birthday Wishes 🎂</h1>
-//       <div className="card-container">
-//         <BdayCard name="R.S" bdayImg={images[0]} />
-//         <RituCard rituName="Billy" rituImg={images[6]} />
-//         <CakeCard kidName="....321" cakeImg={images[1]} />
-//         <RituCard rituName="Billy" rituImg={images[7]} />
-//         <BdayCard name="R.S" bdayImg={images[2]} />
-//         <RituCard rituName="Billy" rituImg={images[8]} />
-//         <CakeCard kidName="....321" cakeImg={images[3]} />
-//         <RituCard rituName="Billy" rituImg={images[9]} />
-//         <BdayCard name="R.S" bdayImg={images[4]} />
-//         <RituCard rituName="Billy" rituImg={images[10]} />
-//         <CakeCard kidName="....321" cakeImg={images[5]} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-// import React, { useState } from "react";
-// import "./App.css";
-// import { BdayCard, CakeCard, RituCard } from "../components/card";
-
-// const imagesPath = "/images/";
-
-// const images = [
-//   `${imagesPath}card1.jpg`, `${imagesPath}card4.jpg`, `${imagesPath}card2.jpg`,
-//   `${imagesPath}card3.jpg`, `${imagesPath}card5.jpg`, `${imagesPath}card6.jpg`,
-//   `${imagesPath}card9.jpg`, `${imagesPath}card14.jpg`, `${imagesPath}card15.jpg`,
-//   `${imagesPath}card10.jpg`, `${imagesPath}card11.jpg`
-// ];
-
-// const App = () => {
-//   const [showCards, setShowCards] = useState(false);
-
-//   const toggleCards = () => {
-//     setShowCards(!showCards);
-//   };
-
-//   return (
-//     <div className="app-container">
-//       <h1 className="title">🎉 Click Below for a Surprise 🎂</h1>
-      
-//       <button className="toggle-btn" onClick={toggleCards}>
-//         {showCards ? "Hide Cards ❌" : "Show Cards 🎁"}
-//       </button>
-
-//       {showCards && (
-//         <div className="card-container">
-//           <BdayCard name="R.S" bdayImg={images[0]} />
-//           <RituCard rituName="Billy" rituImg={images[6]} />
-//           <CakeCard kidName="....321" cakeImg={images[1]} />
-//           <RituCard rituName="Billy" rituImg={images[7]} />
-//           <BdayCard name="R.S" bdayImg={images[2]} />
-//           <RituCard rituName="Billy" rituImg={images[8]} />
-//           <CakeCard kidName="....321" cakeImg={images[3]} />
-//           <RituCard rituName="Billy" rituImg={images[9]} />
-//           <BdayCard name="R.S" bdayImg={images[4]} />
-//           <RituCard rituName="Billy" rituImg={images[10]} />
-//           <CakeCard kidName="....321" cakeImg={images[5]} />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default App;
-
+//////////////////
 
 
 import React, { useState } from "react";
 import "./App.css";
 import { BdayCard, CakeCard, RituCard } from "../components/card";
 
-const imagesPath = "/images/";
+const imagesPath = "../images/";
 
 const images = [
   `${imagesPath}card1.jpg`, `${imagesPath}card4.jpg`, `${imagesPath}card2.jpg`,
